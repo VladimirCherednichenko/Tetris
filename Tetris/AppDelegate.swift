8cb84179fd95ade687a1c673a43538a6ed04fb85
VladimirCherednichenko <cherid95@gmail.com>
2017-01-19 19:13:33 +0300

    added line of views

--- Tetris/AppDelegate.swift
+++ Tetris/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let windowFrame = UIScreen.main.bounds
+        self.window = UIWindow(frame: windowFrame)
+        
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
--- Tetris/ViewController.swift
+++ Tetris/ViewController.swift
@@ -12,9 +12,72 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.white
+        
+        
+        
+        let verticalStack = UIStackView()
+        verticalStack.axis = .vertical
+        
+        verticalStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+      
+        view.addSubview(verticalStack)
+        
+        
+        
+        verticalStack.rightAnchor.constraint(equalTo: view.rightAnchor).isActive=true
+        verticalStack.leftAnchor.constraint(equalTo: view.leftAnchor).isActive=true
+        verticalStack.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive=true
+        verticalStack.topAnchor.constraint(equalTo: view.topAnchor).isActive=true
+        
+        
+        
+        
+        let horizontalStack = UIStackView()
+        horizontalStack.spacing=1
+        horizontalStack.axis = .horizontal
+         horizontalStack.translatesAutoresizingMaskIntoConstraints = false
+        verticalStack.addSubview(horizontalStack)
+        
+        //there pixel begins to create
+        var pixelArray=[UIView]()
+        
+        for i in 0...9 {
+        let pixel=UIView()
+        pixel.translatesAutoresizingMaskIntoConstraints = false
+        pixel.backgroundColor=UIColor.blue
+            
+        pixelArray.append(pixel)
+            horizontalStack.addArrangedSubview(pixelArray[i])
+            
+            pixelArray[i].heightAnchor.constraint(equalTo: view.widthAnchor, multiplier:0.1).isActive=true
+        pixelArray[i].widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.1).isActive=true
+           }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
