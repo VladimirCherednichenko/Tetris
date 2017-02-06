d7d356a6a6196b660d1af4bf3f1697318c8aa983
VladimirCherednichenko <cherid95@gmail.com>
2017-02-06 14:55:54 +0300

    was added class Sender

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Tetris
+//
+//  Created by Admin on 03.02.17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import Foundation
---
+++ Sender.swift
@@ -0,0 +1,9 @@
+//
+//  Sender.swift
+//  Tetris
+//
+//  Created by Admin on 06.02.17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import Foundation
