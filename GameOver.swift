09431803645bf0ca774f38214a099c991b8abb71
VladimirCherednichenko <cherid95@gmail.com>
2017-02-08 19:39:22 +0300

    added new colors for Game Over

--- GameOver.swift
+++ GameOver.swift
@@ -26,24 +26,16 @@ class GameOver: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "pixelDark"))
-        let verticalStack=UIStackView()
-        
-        verticalStack.translatesAutoresizingMaskIntoConstraints=false
-        verticalStack.axis = .vertical
        
         let gameOverLabel=UILabel()
         gameOverLabel.text="Game Over"
-        gameOverLabel.textColor=UIColor(patternImage: #imageLiteral(resourceName: "pixelRed"))
+        gameOverLabel.textColor=UIColor.white
         gameOverLabel.textAlignment = .center
         gameOverLabel.font=UIFont(name: "XPED Shadow", size: 60.0   )
-        
-        
-        
         gameOverLabel.translatesAutoresizingMaskIntoConstraints=false
         
         
-        let restartButton=UIButton()
-        restartButton.translatesAutoresizingMaskIntoConstraints=false
+        
         
         
         
@@ -51,14 +43,16 @@ class GameOver: UIViewController {
         view.addSubview(gameOverLabel)
         gameOverLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive=true
         gameOverLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive=true
+        let restartButton=UIButton()
+        restartButton.translatesAutoresizingMaskIntoConstraints=false
+        restartButton.setTitleColor(UIColor.red, for: .highlighted)
         view.addSubview(restartButton)
         restartButton.topAnchor.constraint(equalTo: gameOverLabel.bottomAnchor).isActive=true
         restartButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive=true
         restartButton.setTitle("Restart", for: .normal)
-       
         restartButton.titleLabel?.font=UIFont(name: "XPED Shadow", size: 45.0)
         restartButton.addTarget(senderObject, action: #selector(senderObject.newGame), for: .touchUpInside)
-        //restartButton.addTarget(self, action: , for: <#T##UIControlEvents#>)
+        
         
     }
     override var prefersStatusBarHidden: Bool {
