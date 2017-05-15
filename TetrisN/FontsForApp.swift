3b059f48eb02bfd527e302532dcbf6cb3b04bbf5
Vladimir <vladimir@MacBook-Pro-admin.local>
2017-05-15 15:04:26 +0300

    new class with fonts

---
+++ TetrisN/FontsForApp.swift
@@ -0,0 +1,16 @@
+//
+//  Fonts.swift
+//  TetrisN
+//
+//  Created by Vladimir on 15.05.17.
+//  Copyright © 2017 Vladimir. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Fonts {
+    
+    
+    let gameOverFont = UIFont(name: "XPED Shadow", size: 60.0)
+}
